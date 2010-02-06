2f9d00dc57f7e02f311e65f9498ce1215e19440f
Joe LeBlanc <contact@jlleblanc.com>
2010-02-06 18:15:41 -0500

    Documented the fields class

--- lib/representation/fields.php
+++ lib/representation/fields.php
@@ -1,6 +1,13 @@
 <?php
 namespace Extool\Representation;
 
+/**
+ * Represents a set of fields, keeping track of human friendly labels and 
+ * field types.
+ *
+ * @package default
+ * @author Joseph LeBlanc
+ */
 class Fields
 {
 	/**
@@ -16,12 +23,31 @@ class Fields
 	 * @var string
 	 */
 	protected $types = array();
-	
-	public function __construct()
+
+	/**
+	 * Accepts an array of field types, keyed by human friendly label
+	 *
+	 * @param array $labels 
+	 * @author Joseph LeBlanc
+	 */
+	public function __construct($fields = array())
 	{
-		
+		if (count($fields)) {
+			foreach ($fields as $label => $type) {
+				$this->setField($label, $type)
+			}
+		}
 	}
 
+	/**
+	 * Adds a field to the fieldset, given the human friendly label and field
+	 * type.
+	 *
+	 * @param string $label 
+	 * @param string $type 
+	 * @return boolean
+	 * @author Joseph LeBlanc
+	 */
 	public function setField($label, $type)
 	{
 		if (!isset($this->names[$label])) {
@@ -33,7 +59,14 @@ class Fields
 		return false;
 	}
 
-	public function removeField()
+	/**
+	 * Removes a field, given the human friendly label
+	 *
+	 * @param string $label 
+	 * @return boolean
+	 * @author Joseph LeBlanc
+	 */
+	public function removeField($label)
 	{
 		if (isset($this->names[$label])) {
 			unset($this->names[$label]);
@@ -44,12 +77,26 @@ class Fields
 		return false;
 	}
 
-	private function getSystemName($label)
+	/**
+	 * Takes a human friendly label and returns system name version
+	 *
+	 * @param string $label 
+	 * @return string
+	 * @author Joseph LeBlanc
+	 */
+	protected function getSystemName($label)
 	{
 		$name = str_replace(' ', '_', $label);
 		return preg_replace('/[^A-Za-z_0-9]/', '', $name);
 	}
 
+	/**
+	 * Allows us to get the names and types without being able to set them
+	 *
+	 * @param string $name 
+	 * @return mixed
+	 * @author Joseph LeBlanc
+	 */
 	public function __get($name)
 	{
 		return $this->$name;
