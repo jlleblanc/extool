ac7dea7f3c5976a9dfd657829c11de0207fe7c6a
Joe LeBlanc <contact@jlleblanc.com>
2010-02-10 18:33:20 -0500

    Documented the model class.

--- lib/representation/model.php
+++ lib/representation/model.php
@@ -2,24 +2,50 @@
 namespace Extool\Representation;
 
 /**
-* 
-*/
+ * Represents a Model. Models require both criteria (a set of fields) and
+ * tables to be fully functional.
+ *
+ * @package default
+ * @author Joseph LeBlanc
+ */
 class Model
 {
 	protected $name;
 	protected $tables;
 	protected $criteria;
 
+	/**
+	 * Requires the system-friendly name for the model.
+	 *
+	 * @param string $name 
+	 * @author Joseph LeBlanc
+	 */
 	function __construct($name)
 	{
 		$this->name = $name;
 	}
 
+	/**
+	 * Used to set the fields to be returned by the model. Requires a Fields 
+	 * object.
+	 *
+	 * @param Fields $fields 
+	 * @return void
+	 * @author Joseph LeBlanc
+	 */
 	public function setCriteria(Fields $fields)
 	{
 		$this->criteria = $fields;
 	}
 
+	/**
+	 * Used to add tables to the model. Will only add the table if one with 
+	 * the same name has not already been added.
+	 *
+	 * @param Table $table 
+	 * @return bool
+	 * @author Joseph LeBlanc
+	 */
 	public function addTable(Table $table)
 	{
 		if (!isset($this->tables[$table->name])) {
@@ -30,6 +56,29 @@ class Model
 		return false;
 	}
 
+	/**
+	 * Returns the status of the model. If tables or criteria are not set, 
+	 * validation fails and false is returned.
+	 *
+	 * @return boolean
+	 * @author Joseph LeBlanc
+	 */
+	public function validate()
+	{
+		if (isset($this->tables) || isset($this->criteria)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Allows access to all properties without allowing write access.
+	 *
+	 * @param string $name 
+	 * @return mixed
+	 * @author Joseph LeBlanc
+	 */
 	public function __get($name)
 	{
 		return $this->$name;
