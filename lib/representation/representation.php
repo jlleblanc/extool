a90e81d9644d61ffa7bc257ebff2b4d8ecabb357
Joe LeBlanc <contact@jlleblanc.com>
2010-02-11 19:22:33 -0500

    Created functions for adding objects to a Representation, with type hinting.

--- lib/representation/representation.php
+++ lib/representation/representation.php
@@ -74,4 +74,78 @@ class Representation
 
 		return true;
 	}
+
+	public function addPublicView(View $view)
+	{
+		if (isset($view->access != 'public')) {
+			throw new Exception("View {$view->name} is not designated for 'public' access.");
+		} else if (isset($this->public_views[$view->name])) {
+			throw new Exception("Public View {$view->name} is already present in this representation");
+		}
+
+		$this->public_views[$view->name] = $view;
+	}
+
+	public function addPublicModel(Model $model)
+	{
+		if (isset($this->public_views[$model->name])) {
+			if (isset($this->public_models[$model->name])) {
+				throw new Exception("Public Model {$model->name} is already present in this representation");
+			}
+
+			$this->public_models[$model->name] = $model;
+		}
+
+		throw new Exception("A matching public view for Model {$model->name} was not found");
+	}
+
+	public function addTable(Table $table)
+	{
+		if (isset($this->tables[$table->name])) {
+			throw new Exception("Table {$table->name} is already present in this representation");
+		}
+
+		$this->tables[$table->name] = $table;
+	}
+
+	public function addAdminView(View $view)
+	{
+		if (isset($view->access != 'admin')) {
+			throw new Exception("View {$view->name} is not designated for 'admin' access.");
+		} else if (isset($this->admin_views[$view->name])) {
+			throw new Exception("Admin View {$view->name} is already present in this representation");
+		}
+
+		$this->admin_views[$view->name] = $view;
+	}
+
+	public function addAdminModel(Model $model)
+	{
+		if (isset($this->admin_views[$model->name])) {
+			if (isset($this->admin_views[$model->name])) {
+				throw new Exception("Admin Model {$model->name} is already present in this representation");
+			}
+
+			$this->admin_models[$model->name] = $model;
+		}
+
+		throw new Exception("A matching admin view for Model {$model->name} was not found");
+	}
+
+	/**
+	 * Adds a data set (represented by a Data object) to the representation. 
+	 * If the representation already has a dataset, a merge is attempted.
+	 *
+	 * @param Data $data 
+	 * @return void
+	 * @author Joseph LeBlanc
+	 */
+	public function addData(Data $data)
+	{
+		if (isset($this->data)) {
+			$this->data->mergeIn($data);
+		} else {
+			$this->data = $data;
+		}
+	}
 }
