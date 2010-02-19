3aa15e6cba0233f365c2ab34d9582f7e746af66e
Joe LeBlanc <contact@jlleblanc.com>
2010-02-19 14:05:15 -0500

    Drastically simplified the AdapterInterface.

--- lib/adapter/adapter_interface.php
+++ lib/adapter/adapter_interface.php
@@ -2,11 +2,5 @@
 namespace Extool\Adapter;
 
 interface AdapterInterface {
-	public function getTables();
-	public function getPublicViews();
-	public function getAdminViews();
-	public function getData();
-	public function getName();
-	public function getPublicModels();
-	public function getAdminModels();
+	public function decorateRepresentation(Representation $representation);
 }
