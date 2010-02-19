e81790cbcff0272ed0784addbf3731b0ddb27da5
Joe LeBlanc <contact@jlleblanc.com>
2010-02-19 14:04:45 -0500

    Adding the first version of the Adapter Interface.

--- lib/adapter/adapter_interface.php
+++ lib/adapter/adapter_interface.php
@@ -1,2 +1,12 @@
 <?php
 namespace Extool\Adapter;
+
+interface AdapterInterface {
+	public function getTables();
+	public function getPublicViews();
+	public function getAdminViews();
+	public function getData();
+	public function getName();
+	public function getPublicModels();
+	public function getAdminModels();
+}
