0fec1628c66668820991ea01d6377a822311b474
Joe LeBlanc <contact@jlleblanc.com>
2010-02-28 19:54:46 -0500

    Added the SnippetFactory class

--- lib/helpers/snippetfactory.php
+++ lib/helpers/snippetfactory.php
@@ -1,2 +1,59 @@
 <?php
 namespace Extool\Helpers;
+
+/**
+ * A Factory for loading up snippet files, then constructing snippet objects;
+ * this should help performance, as well as keep the file loading aspect away
+ * from the core Snippet class
+ *
+ * @package default
+ * @author Joseph LeBlanc
+ */
+class SnippetFactory
+{
+	private $snippet_contents = array();
+	private $base_path = false;
+
+	/**
+	 * Given a filename, this function returns a corresponding snippet object.
+	 * It first checks to see if the snippet text has been loaded from disk. If
+	 * so, it assembles a Snippet object using the cached text. Otherwise, the
+	 * text is loaded and cached first.
+	 *
+	 * @param string $filename 
+	 * @return void
+	 * @author Joseph LeBlanc
+	 */
+	public function getSnippet($filename)
+	{
+		if (!isset($this->snippet_contents[$filename])) {
+
+			if ($this->base_path) {
+				$full_file_path = $this->base_path . '/' . $filename;
+			} else {
+				$full_file_path = $filename;
+			}
+
+			$this->snippet_contents[$filename] = file_get_contents($full_file_path);
+		}
+
+		return new Snippet($this->snippet_contents[$filename]);
+	}
+
+	/**
+	 * Used to set the root directory to start looking for snippets. Do not add
+	 * the trailing slash.
+	 *
+	 * @param string $path 
+	 * @return void
+	 * @author Joseph LeBlanc
+	 */
+	public function setBasePath($path)
+	{
+		if(is_dir($path)) {
+			$this->base_path = $path;
+		} else {
+			throw new \Exception("Path {$path} does not exist");
+		}
+	}
+}
