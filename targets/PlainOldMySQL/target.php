522e241e367c905c70d8241e7912a43d20f2ef1d
Joe LeBlanc <contact@jlleblanc.com>
2010-03-09 21:18:00 -0500

    Added DROP TABLE snippet.

--- targets/PlainOldMySQL/target.php
+++ targets/PlainOldMySQL/target.php
@@ -25,15 +25,18 @@ class PlainOldMySQL implements \Extool\Target\TargetInterface
 		$files = new \Extool\Helpers\FilePackage();
 
 		$install_file = new \Extool\Helpers\File();
+		$uninstall_file = new \Extool\Helpers\File();
 
 		foreach ($this->rep->tables as $table) {
 			$table->createDefaultKey();
 
 			$mysql = new \Extool\Helpers\MySQL($table);
 			$install_file->appendContents($mysql->generateCreate() . "\n\n");
+			$uninstall_file->appendContents($mysql->generateDrop() . "\n");
 		}
 
 		$files->addFile('admin/install.mysql.sql', $install_file);
+		$files->addFile('admin/uninstall.mysql.sql', $uninstall_file);
 
 		return $files;
 	}
