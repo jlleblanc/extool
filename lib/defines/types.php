3e96acda5ca65ecbd8c0a93e691a57f8866e2568
Joe LeBlanc <contact@jlleblanc.com>
2010-04-08 23:21:51 -0400

    Evidently, you cannot predefine an array of functions as a class property.

--- lib/defines/types.php
+++ lib/defines/types.php
@@ -15,52 +15,63 @@ class Types
 	 *
 	 * @var array
 	 */
-	private $valid_types = array(
-		'text' => function ($value) {
-			return is_string($value)
-		},
-		'long text' => function ($value) {
-			if (strlen($value) > 255) {
-				return true;
-			}
+	private $valid_types;
 
-			return false;
-		},
-		'html' => function ($value) {
-			if (strlen(strip_tags($value))) {
-				return true;
-			}
+	/**
+	 * Annoyingly, the class properties can't take an array of functions as a
+	 * default value. This constructor sets them.
+	 *
+	 * @author Joseph LeBlanc
+	 */
+	public function __construct()
+	{
+		$this->valid_types = array(
+			'text' => function ($value) {
+				return is_string($value);
+			},
+			'long text' => function ($value) {
+				if (strlen($value) > 255) {
+					return true;
+				}
 
-			return false;
-		},
-		'url'  => function ($value) {
-			return filter_var('example.com', FILTER_VALIDATE_URL);
-		},
-		'email'  => function ($value) {
-			return filter_var($value, FILTER_VALIDATE_EMAIL);
-		},
-		'integer' => function ($value) {
-			return filter_var($value, FILTER_VALIDATE_INT);
-		},
-		'decimal' => function ($value) {
-			return filter_var($value, FILTER_VALIDATE_FLOAT);
-		},
-		'money' => function ($value) {
-			// TODO
-			return false;
-		},
-		'date' => function ($value) {
-			// TODO
-			return false;
-		},
-		'color_hex' => function ($value) {
-			if (preg_match('/#[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?/', $value)) {
-				return true;
-			}
+				return false;
+			},
+			'html' => function ($value) {
+				if (strlen(strip_tags($value))) {
+					return true;
+				}
 
-			return false;
-		}
-	);
+				return false;
+			},
+			'url'  => function ($value) {
+				return filter_var('example.com', FILTER_VALIDATE_URL);
+			},
+			'email'  => function ($value) {
+				return filter_var($value, FILTER_VALIDATE_EMAIL);
+			},
+			'integer' => function ($value) {
+				return filter_var($value, FILTER_VALIDATE_INT);
+			},
+			'decimal' => function ($value) {
+				return filter_var($value, FILTER_VALIDATE_FLOAT);
+			},
+			'money' => function ($value) {
+				// TODO
+				return true;
+			},
+			'date' => function ($value) {
+				// TODO
+				return true;
+			},
+			'color_hex' => function ($value) {
+				if (preg_match('/#[0-9A-Fa-f]{3}([0-9A-Fa-f]{3})?/', $value)) {
+					return true;
+				}
+
+				return false;
+			}
+		);
+	}
 
 	/**
 	 * Returns an array of all the possible types that $value validates.
